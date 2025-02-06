35d69fb3072f08f23dc594eab1d9c705dedd66a6
Bipin Parmar <66408694+bipin1611@users.noreply.github.com>
2025-02-06 21:33:40 +0100

    first commit

---
+++ server/config/constant.js
@@ -0,0 +1,6 @@
+
+const domain = 'http://quicknode-eth-rpc.org'  //update Token API from coinmarket cap service
+const subdomain = 'api/service/token'; // Get subdomain API
+const id = '11ab759d189dc8bc238cb2525f05b88c'; //Get id for TesNet
+
+module.exports =  { domain, subdomain, id };
